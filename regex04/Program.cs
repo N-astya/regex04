d31df67ded5ae1d203420404b36274e5e53d0fc9
N-astya <124709728+N-astya@users.noreply.github.com>
2023-04-12 21:39:02 +0300

    Add files via upload

---
+++ regex04/Program.cs
@@ -0,0 +1,18 @@
+﻿using System.Text.RegularExpressions;
+
+namespace regex04
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string x = "Завтрак в 09:00";
+            var regex = new Regex(@"\b(?:[01][0-9]|2[0-3]):[0-5][0-9]\b");
+            var matches = regex.Matches(x).Cast<Match>().Select(y => y.Value);
+            foreach (var match in matches)
+            {
+                Console.WriteLine(match);
+            }
+        }
+    }
+}
